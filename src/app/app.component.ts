6805e062dd46004d94f9d16c5e37d484fe4247fb
vitmar.aliaga <vitmar.aliaga>
2019-03-22 12:25:31 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mcaj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mcaj-coordina';
+}
